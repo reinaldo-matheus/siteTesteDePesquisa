15a9b2b05f14a31d0cee8a17092d928723fcb358
Matheus Reinaldo <160988681+reinaldo-matheus@users.noreply.github.com>
2024-09-06 19:27:15 -0300

    Utilizando ferramentas HTML, CSS, JavaScript
    foi desenvolvida uma página simples em HTML , estilizada com CSS, e buscando deixar a mesma dinâmica, foi utilizado JavaScript.

---
+++ atividade01-uninassau/dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo: "Chris Bumstead",
+        descricao: "Atleta de fisiculturismo",
+        link:"https://en.wikipedia.org/wiki/Chris_Bumstead",
+        tags:"fisiculturismo cbum olympia"
+    },
+
+    {
+        titulo: "Philipe Coutinho",
+        descricao: "Atleta profissional de futebol",
+        link:"https://pt.wikipedia.org/wiki/Philippe_Coutinho",
+        tags:"futebol vasco futebolista coutinho"
+    },
+
+    {
+        titulo: "Lebron James",
+        descricao: "Atleta profissional de Basquete",
+        link:"https://en.wikipedia.org/wiki/LeBron_James",
+        tags:"basketball basquete nba "
+    }
+
+];
